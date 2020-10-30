f9289121c36c35f5346d0b937eaf4aa252bf24f8
Atul1023 <07atul1023@gmail.com>
2020-10-30 17:06:54 +0530

    Face detection functionality implemented

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,26 @@
 import React from "react";
+import "./FaceRecognition.css";
 
-const FaceRecognition = ({ imageURL }) => {
+const FaceRecognition = ({ imageURL, box }) => {
   return (
     <div className="center ma">
       <div className="absolute mt2">
-        <img alt={""} src={imageURL} width="500px" height="auto" />
+        <img
+          id="inputimage"
+          alt={""}
+          src={imageURL}
+          width="500px"
+          height="auto"
+        />
+        <div
+          className="bounding-box"
+          style={{
+            left: box.leftCol,
+            right: box.rightCol,
+            top: box.topRow,
+            bottom: box.bottomRow,
+          }}
+        ></div>
       </div>
     </div>
   );
